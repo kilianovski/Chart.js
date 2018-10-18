1aeaab7bc904c770e6ce7d75858650144fbbc100
mikhail kilianovski <mitutee@gmail.com>
2018-10-18 16:05:10 +0300

    Add 'align' option for legend

--- src/plugins/plugin.legend.js
+++ src/plugins/plugin.legend.js
@@ -430,7 +430,10 @@ var Legend = Element.extend({
 				var width = boxWidth + (fontSize / 2) + textWidth;
 				var x = cursor.x;
 				var y = cursor.y;
-
+				
+				if (me.options.align === 'center')
+					y += (ctx.canvas.clientHeight - (itemHeight * me.legendItems.length)) / 2;
+				
 				if (isHorizontal) {
 					if (x + width >= legendWidth) {
 						y = cursor.y += itemHeight;
